4aed63f57d95b4e2e2da4e502768977eff6f0654
momomo0505 <techcamp.fujita.t@gmail.com>
2023-07-20 22:12:35 +0900

    マイページ実装

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :items do
     resources :orders, only: [:index, :create] 
     end
-
+    resources :users, only: :show
+    
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
