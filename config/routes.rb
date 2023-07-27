75c3650135102199984f5cd66c2906337732594a
momomo0505 <techcamp.fujita.t@gmail.com>
2023-07-27 22:40:58 +0900

    一旦保存

--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
 
-
-  
   resources :items do
+    resources :comments, only: :create
     delete 'remove_item/:item_id', to: 'users#remove_item', as: 'remove_item'
     resources :orders, only: [:index, :create] 
     member do
       post :add_to_cart
     end
     end
+
     resources :users, only: :show
-    
-    
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
+   end
