8ccd929c90934c4f1c841818ea0a60391b3d2217
momomo0505 <techcamp.fujita.t@gmail.com>
2023-07-29 00:46:39 +0900

    一部修正

--- app/models/days_to_ship.rb
+++ app/models/days_to_ship.rb
@@ -3,8 +3,8 @@ class DaysToShip < ActiveHash::Base
     { id: 0, name: '---' },
     { id: 1, name: '1~2日で発送' },
     { id: 2, name: '2~3日で発送' },
-    { id: 3, name: '4~7日で発送' },
-    { id: 4, name: '8/15以降で発送' },
+    { id: 3, name: 'お打ち合わせの上' },
+    { id: 4, name: '8/11以降で発送' },
   ]
 
   include ActiveHash::Associations
--- app/models/shipping_charge.rb
+++ app/models/shipping_charge.rb
@@ -1,9 +1,9 @@
 class ShippingCharge < ActiveHash::Base
   self.data = [
     { id: 0, name: '---' },
-    { id: 1, name: '直接引取のみ、発送不可' },
-    { id: 2, name: '発送可(送料込み)、直接引取可' },
-    { id: 3, name: '発送のみ、直接引取不可' },
+    { id: 1, name: '直接引取(発送不可)' },
+    { id: 2, name: '発送(直接引取可)' },
+    { id: 3, name: '発送もしくは直接引取' },
   ]
 
   include ActiveHash::Associations
