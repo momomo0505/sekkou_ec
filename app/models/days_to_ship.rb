06621eafe7ca2659cc1df32f3c2c74c58ac33487
momomo0505 <techcamp.fujita.t@gmail.com>
2023-07-29 11:43:15 +0900

    直接引き取り、発送選択機能実装

--- app/models/days_to_ship.rb
+++ app/models/days_to_ship.rb
@@ -1,9 +1,9 @@
 class DaysToShip < ActiveHash::Base
   self.data = [
     { id: 0, name: '---' },
-    { id: 1, name: '1~2日で発送' },
-    { id: 2, name: '2~3日で発送' },
-    { id: 3, name: 'お打ち合わせの上' },
+    { id: 1, name: '送料込み' },
+    { id: 2, name: '送料別途' },
+    { id: 3, name: 'ー' },
     { id: 4, name: '8/11以降で発送' },
   ]
 
