f875bbb26fa49ff0eab43e9f040485fd18334c40
momomo0505 <techcamp.fujita.t@gmail.com>
2023-07-30 12:31:23 +0900

    スマホ表示色々修正

--- app/models/shipping_charge.rb
+++ app/models/shipping_charge.rb
@@ -3,7 +3,7 @@ class ShippingCharge < ActiveHash::Base
     { id: 0, name: '---' },
     { id: 1, name: '直接引取(発送不可)' },
     { id: 2, name: '発送(直接引取可)' },
-    { id: 3, name: '発送もしくは直接引取' },
+    { id: 3, name: '発送or直接引取' },
   ]
 
   include ActiveHash::Associations
